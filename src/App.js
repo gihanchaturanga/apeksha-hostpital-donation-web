c4a8a0fcce54229b356d4d68a7f78ca12fd21426
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-02-28 22:11:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './top_nav/TopNav';
-import React from 'react';
 
 import RenderHome from './Main/Main';
 

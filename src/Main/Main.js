cdcb064176084c0b51b52c008e0bdc6b673f9419
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-02-28 22:31:43 +0530

    main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -6,12 +6,11 @@ function RenderHome(){
     return(
         <div >
             <ResponsiveAppBar/>
-            <Grid container sx={{backgroundColor:"F7EDE4"}}>
-                <Grid xs={12} md={6}>
-                    <Item><img src="../resources/images/main.jpg"/></Item>
+            <Grid container sx={{backgroundColor:"F7EDE4", height:"100%"}}>
+                <Grid item xs={12} md={6} sx={{backgroundColor:"red"}}>
+                    <img src="../resources/images/main.jpg"/>
                 </Grid>
-                <Grid xs={12} md={6}>
-                    <Item></Item>
+                <Grid item xs={12} md={6}>
                 </Grid>
             </Grid>
         </div>

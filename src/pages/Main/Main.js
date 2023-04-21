4235fe9833728fe652133b7589b0ab180efe470f
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-04-21 22:43:09 +0530

    day 01

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ResponsiveAppBar from "../../common/top_nav/TopNav";
-import './Main.css'
-import bgImg from '../../resources/images/main.jpg'
+import "./Main.css";
+import { Grid } from "@mui/material";
+import { Height } from "@mui/icons-material";
 
-function RenderHome(){
-    return(
-        <div id="main-container">
-            <ResponsiveAppBar/>
-            <div className="body-container" style={{border: '5px solid black', width:'500px', height:'500px'}}>
-
-            </div>
-        </div>
-    );
+function RenderHome() {
+  return (
+    <div container direction="column" className="main-container">
+      <ResponsiveAppBar />
+      <Grid container xs={12} className="body-container" direction="row">
+        <Grid item xs={8} className="bg-img"></Grid>
+        <Grid item xs></Grid>
+      </Grid>
+    </div>
+  );
 }
-export default ResponsiveAppBar;
+export default RenderHome;

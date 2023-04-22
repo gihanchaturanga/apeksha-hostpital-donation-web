88439d0857b680b58d3cdfe0280c384118e903cf
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-04-22 15:39:46 +0530

    day 01

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import NewTextField from "../../common/TextField/NewTextField";
+import { Grid, Button } from "@mui/material";
+import "./Login.css";
+import logo from "../../resources/logo/VolunteerSL(dark).png";
 
 export const Login = () => {
   return (
-    <div>
-        <div className='container'>
-            <h1>Login</h1>
+    <Grid container direction="row" className="main-container">
+      <Grid item xs={12} md={5} className="login-section grid-item">
+        <img id="logo" src={logo} alt="Volunteer Logo" />
+        <span className="main-txt">Volunteer Sri Lanka</span>
+        <div className="container">
+          <div className="login-field">
+            {/* <label htmlFor="email">E-mail</label> */}
+            <NewTextField
+              label="E-mail"
+              id="email"
+              placeholder="abc@gmail.com"
+              name="email"
+            />
+          </div>
+          <div className="login-field">
+            {/* <label htmlFor="password">Password</label> */}
+            <NewTextField
+              label="Password"
+              id="password"
+              type="password"
+              placeholder="Your Password"
+              name="password"
+            />
+          </div>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#4C3B2BF0", color: "#F6EDE4" }}
+          >
+            Login
+          </Button>
         </div>
-    </div>
-  )
-}
+      </Grid>
+      <Grid item xs={{ display: "none" }} md className="img grid-item"></Grid>
+    </Grid>
+  );
+};

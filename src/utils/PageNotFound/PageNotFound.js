599539e3acad0293f5e33fd43996cca66af36378
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-04-23 16:03:42 +0530

    day 02

--- src/utils/PageNotFound/PageNotFound.js
+++ src/utils/PageNotFound/PageNotFound.js
@@ -1,12 +1,36 @@
 import React from "react";
-import './PageNotFound.css'
+import "./PageNotFound.css";
+import { Grid, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const PageNotFound = () => {
+  const nav = useNavigate();
   return (
-    <div className="pnf-container">
-      <h1 className="pnf-h1">Page Not Found!</h1>
-      <p className="pnf-p">The page you're looking for does not exist or has been moved.</p>
-      <a href="/">Return to Homepage</a>
-    </div>
+    <Grid
+      className="pnf-container"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <div>
+        <div className="pnf-back-img">
+          <h1 className="pnf-h1">Oops!</h1>
+        </div>
+        <h3 className="pnf-h3">404 - PAGE NOT FOUND</h3>
+        <p className="pnf-p">
+          The page you're looking for does not exist or has been moved.
+        </p>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#4C3B2BF0" }}
+          id="back-to-home"
+          onClick={() => {
+            nav("/");
+          }}
+        >
+          Return to Homepage
+        </Button>
+      </div>
+    </Grid>
   );
 };

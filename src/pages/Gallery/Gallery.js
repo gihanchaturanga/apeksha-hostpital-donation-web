4d346ee1ce60c1e969a6374f374dadb2d72a8d3a
gihanchaturanga <gihanchaturanga98@gmail.com>
2023-04-24 13:48:05 +0530

    title things

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -11,7 +11,7 @@ export default function MasonryImageList() {
 
   return (
     <>
-    <Typography variant='h3' marginY={3}>{title}</Typography>
+    <Typography variant='h3' sx={{color: "#4C3B2BF0", textAlign: 'center'}} marginY={3}>{title}</Typography>
       <Box sx={{ overflowY: 'scroll', paddingLeft: 2, paddingRight: 2 }}>
         <ImageList variant="masonry" cols={3}>
           {itemData.map((item) => item.collection == id ? (

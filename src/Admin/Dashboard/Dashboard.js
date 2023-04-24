0c3b641df5a834b45c1dae592ded711953cf072e
gihanchaturanga <gihanchaturanga98@gmail.com>
2023-04-24 19:25:39 +0530

    theme-color setting

--- src/Admin/Dashboard/Dashboard.js
+++ src/Admin/Dashboard/Dashboard.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import ResponsiveDrawer from '../../common/AdminNav/AdminNav'
-import { ThemeProvider, createTheme } from '@mui/material'
-import { orange } from '@mui/material/colors'
+import { ThemeProvider, Typography, createTheme } from '@mui/material'
+import { amber, blueGrey, brown, deepOrange, green, grey, orange } from '@mui/material/colors'
 
 const Dashboard = () => {
 
     const theme = createTheme({
         palette: {
             primary:{
-                main: '#4C3B2B'
+                light: brown[600],
+                main: brown[800],
+                dark: brown[900]
+            },
+            secondary:{
+                light: blueGrey[500],
+                main: blueGrey[700],
+                dark: blueGrey[800]
+
+            },
+            success: {
+                light: green[600],
+                main: green[700],
+                dark: green[800]
+            },
+            warning:{
+                light: amber[400],
+                main: amber[600],
+                dark: amber[800]
+            },
+            error: {
+                light: deepOrange[800],
+                main: '#dd2c00',
+                dark: deepOrange[900]
             }
         }
     })
@@ -17,7 +40,10 @@ const Dashboard = () => {
   return (
     <>
         <ThemeProvider theme={theme}>
-            <ResponsiveDrawer/>
+            <ResponsiveDrawer>
+            <Typography variant='h1' color={'primary.light'}>Here we go</Typography>
+            </ResponsiveDrawer>
+            
         </ThemeProvider>
     </>
   )

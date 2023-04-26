8597f359d5d937ff7396c311722e4c0c6cedad7e
Denuwan Bandara <86357900+denam98@users.noreply.github.com>
2023-04-26 15:28:39 +0530

    Day 05

--- src/common/DonateComponent/DonateComponent.js
+++ src/common/DonateComponent/DonateComponent.js
@@ -1,21 +1,184 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Button, CardActions } from "@mui/material";
+import {
+  Button,
+  CardActions,
+  ToggleButtonGroup,
+  ToggleButton,
+  Grid,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./DonateComponent.css";
 
 export const DonateComponent = () => {
+  const [type, setType] = useState("one-time");
+  const [value, setvalue] = useState("5000");
+
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleValueChange = (event) => {
+    setvalue(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }} elevation="5">
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        {/* Button group for donation type */}
+        <ToggleButtonGroup
+          value={type}
+          exclusive
+          onChange={handleTypeChange}
+          aria-label="Platform"
+          className="toggle"
+          size="small"
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <ToggleButton
+            value="one-time"
+            variant="outlined"
+            id={type === "one-time" ? "selected" : "normal"}
+            sx={{ paddingX: "25px", boxSizing: "border-box" }}
+          >
+            One Time
+          </ToggleButton>
+          <ToggleButton
+            value="monthly"
+            variant="outlined"
+            id={type === "monthly" ? "selected" : "normal"}
+            sx={{ paddingX: "10px", boxSizing: "border-box" }}
+          >
+            <FavoriteIcon
+              color="error"
+              sx={{ paddingX: 0.45, marginRight: 0.5 }}
+              value="monthly"
+              disabled
+            />
+            Monthly
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <Typography
+          sx={{ fontSize: 18, paddingTop: 4 }}
+          color="text.secondary"
+          gutterBottom
+        >
+          Donate to Apeksha Cancer Hospital
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+
+        {/* Button Group for donate amount */}
+        <ToggleButtonGroup
+          value={value}
+          exclusive
+          onChange={handleValueChange}
+          aria-label="Platform"
+          className="toggle"
+          size="small"
+          sx={{
+            display: "flex",
+            flexFlow: "row wrap",
+            justifyContent: "space-evenly",
+            rowGap:'10px'
+          }}
+        >
+          <ToggleButton
+            value="2000"
+            variant="outlined"
+            id={value === "2000" ? "selected" : "normal"}
+            xs={4}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 2,000
+          </ToggleButton>
+          <ToggleButton
+            item
+            value="5000"
+            variant="outlined"
+            id={value === "5000" ? "selected" : "normal"}
+            xs={4}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 5,000
+          </ToggleButton>
+          <ToggleButton
+            item
+            value="8000"
+            variant="outlined"
+            id={value === "8000" ? "selected" : "normal"}
+            xs={4}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 8,000
+          </ToggleButton>
+          <ToggleButton
+            item
+            value="12000"
+            variant="outlined"
+            id={value === "12000" ? "selected" : "normal"}
+            xs={4}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 12k
+          </ToggleButton>
+          <ToggleButton
+            item
+            value="15000"
+            variant="outlined"
+            id={value === "15000" ? "selected" : "normal"}
+            xs={4}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 15k
+          </ToggleButton>
+          <ToggleButton
+            item
+            value="20000"
+            variant="outlined"
+            id={value === "20000" ? "selected" : "normal"}
+            sx={{
+              paddingX: "10px",
+              width:"90px",
+              boxSizing: "border-box",
+              fontSize: "15px",
+            }}
+          >
+            Rs 20k
+          </ToggleButton>
+        </ToggleButtonGroup>
+        
       </CardContent>
       <CardActions sx={{ justifyContent: "center" }}>
         <Button
